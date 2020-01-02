ec284ac1e5bf95c9e077a88ae51a387490dfa168
Noah Clark <noahbc08@gmail.com>
2020-01-01 22:04:42 -0500

    re did files

---
+++ precache-manifest.76e31ca574ea90a3c350fd5017c18758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfb7766170e9c6811d70b0673d87f4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb140243d1cd692ee8a",
+    "url": "/static/css/main.07bb9391.chunk.css"
+  },
+  {
+    "revision": "02aa8c82ffb9056e5301",
+    "url": "/static/js/2.98a6619b.chunk.js"
+  },
+  {
+    "revision": "fd56044fbab90a74509299bbf81211c5",
+    "url": "/static/js/2.98a6619b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eeb140243d1cd692ee8a",
+    "url": "/static/js/main.a565b0e2.chunk.js"
+  },
+  {
+    "revision": "a541759971627da6b625",
+    "url": "/static/js/runtime-main.ed667afe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "clarknoah.github.io/home/precache-manifest.19c0736fda26a17b1a563a9624eddd5a.js"
+  "/precache-manifest.76e31ca574ea90a3c350fd5017c18758.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("clarknoah.github.io/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
